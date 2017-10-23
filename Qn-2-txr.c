9da69eaa1bfd38ae0a47e64458b8d8b3ea0c5707
Amal Poulose <amalpoulose1995@hotmail.com>
2017-10-23 10:18:08 +0530

    Add files via upload

---
+++ Qn-2-txr.c
@@ -0,0 +1,42 @@
+/*2.modify above programm in such a way that sort the data according to descending order.then send
+back the o/p to process1.
+O/p:
+vtroniiedca
+87421*/
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+int main(void)
+{
+	mkfifo("p",0664);
+	perror("pipe1");
+	mkfifo("q",0664);
+	perror("pipe2");
+
+	int i,pd,qd,i_arr[20];
+	char c_arr[20];
+	pd=open("p",O_WRONLY);
+	qd=open("q",O_RDONLY);
+	char s[20];
+
+	printf("Enter a string : ");
+	scanf("%s",s);
+
+	write(pd,s,strlen(s)+1);
+	read(qd,c_arr,sizeof(c_arr));
+	printf("sorted character array : %s\n",c_arr);
+	
+        read(qd,i_arr,sizeof(i_arr));
+        printf("sorted integer array : ");
+	for(i=0;i_arr[i]!=-1;i++)
+           printf("%d, ",i_arr[i]);
+
+	printf("\b  \n   Done  \n");
+	return 0;
+}
+
