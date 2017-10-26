de2bd50246f60274b33a8ffcbc7533610cbb615b
Amal Poulose <amalpoulose1995@hotmail.com>
2017-10-26 13:17:47 +0530

    Update Qn-2-rxr.c

--- Qn-2-rxr.c
+++ Qn-2-rxr.c
@@ -21,21 +21,22 @@ int main(void)
 	int i,j,k,l,pd,qd,i_arr[20];
 	pd=open("p",O_RDONLY);
 	qd=open("q",O_WRONLY);
-	char s[20],c_arr[20];
+	char s[100],c_arr[100];
 
 	read(pd,s,sizeof(s));
 	printf("string is %s\n",s);
-	for(i=0,j=0,k=0;s[i];i++)
+	for(i=0,j=1,k=0;s[i];i++)
 		if(s[i]>=48 && s[i]<=57)
 			i_arr[j++]=s[i]-48;
 		else if((s[i]>=65 && s[i]<= 90)||(s[i]>=97 && s[i]<=122))
 			c_arr[k++]=s[i];
+        i_arr[0]=j;
 	printf("Character array : ");
 	for(i=0;i<k;i++)
 		printf("%c",c_arr[i]);
 
 	printf("\nInteger array  :  ");
-	for(i=0;i<j;i++)
+	for(i=1;i<j;i++)
 		printf("%d ,",i_arr[i]);
 
 	for(i=0;i<k-1;i++)
@@ -43,16 +44,15 @@ int main(void)
 		if(c_arr[l]<c_arr[l+1])
 			c_arr[l]=c_arr[l]+c_arr[l+1]-(c_arr[l+1]=c_arr[l]);
 	c_arr[k]='\0';
-	write(qd,c_arr,strlen(c_arr));
-	for(i=0;i<j-1;i++)
-	for(l=0;l<j-i-1;l++)
+	write(qd,c_arr,strlen(c_arr)+1);
+	for(i=1;i<=j-1;i++)
+	for(l=1;l<=j-i-1;l++)
 		if(i_arr[l]<i_arr[l+1])
 			i_arr[l]=i_arr[l]+i_arr[l+1]-(i_arr[l+1]=i_arr[l]);
-        i_arr[j]=-1;       
-	write(qd,i_arr,sizeof(int)*(j+1));
-
-	printf("\b  \n   Done  \n");
+               
+	write(qd,i_arr,sizeof(int)*(i_arr[0]+1));
+	read(pd,s,sizeof(s));
+	printf("\b  \n  Done  \n");
 	return 0;
 }
-
 
